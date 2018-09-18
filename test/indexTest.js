24f58a9341ef150321f746e5d4f936cf76b08216
maxwellbenton <maxwellbenton@gmail.com>
2018-09-18 09:21:38 -0400

    updated tests, packages

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,131 +1,120 @@
+const chai = require('chai');
+global.expect = chai.expect;
+
+const fs = require('file-system');
+const jsdom = require('mocha-jsdom');
+const path = require('path');
+
+const html = fs.readFileSync(
+	path.resolve(__dirname, '..', 'index.html'),
+	'utf-8'
+);
+
+jsdom({
+	html
+});
+
 describe('index.html', () => {
-	it('contains an `p` nested inside a `div`', () => {
-		expect(
-			document.querySelectorAll('body').length,
-			'No `body` tag was found'
-		).to.be.above(0);
-		expect(
-			document.querySelector('body').outerHTML,
-			'No `h1` tag was found inside `body`'
-		).to.match(/<h1>[\s\S]*?<\/h1>/);
+	let body;
+	beforeEach(function() {
+		chai.use(require('chai-dom'));
+		body = document.querySelector('body');
 	});
 
-	it('contains an `li` tag nested inside a `ul`', () => {
-		expect(
-			document.querySelectorAll('ul').length,
-			'No `ul` tag was found'
-		).to.be.above(0);
-		expect(
-			document.querySelector('ul').outerHTML,
-			'No `li` tag was found inside a `ul`'
-		).to.match(/<li>[\s\S]*?<\/li>/);
+	it('contains an `h1` nested inside `body`', () => {
+		let html = document.querySelector('html');
+
+		expect(html, 'No `body` tag was found').to.contain('body');
+		expect(body, 'No `h1` tag was found inside `body`').to.contain('h1');
+	});
+
+	it('contains an `li` tag nested inside a `ul` tag', () => {
+		let ul = document.querySelector('ul');
+
+		expect(body, 'No `ul` tag was found').to.contain('ul');
+		expect(ul, 'No `li` tag was found inside a `ul` tag').to.contain('li');
 	});
 
 	it('contains a `span` tag inside a `main` tag', () => {
-		expect(
-			document.querySelectorAll('main').length,
-			'No `main` tag was found'
-		).to.be.above(0);
-		expect(
-			document.querySelector('main').outerHTML,
-			'No `span` tag was found inside a `main` tag'
-		).to.match(/<span>[\s\S]*?<\/span>/);
+		let main = document.querySelector('main');
+
+		expect(body, 'No `main` tag was found').to.contain('main');
+		expect(main, 'No `span` tag was found inside a `main` tag').to.contain(
+			'span'
+		);
 	});
 
 	it('contains a `header` tag inside an `article` tag', () => {
+		let article = document.querySelector('article');
+
+		expect(body, 'No `article` tag was found').to.contain('article');
 		expect(
-			document.querySelectorAll('article').length,
-			'No `article` tag was found'
-		).to.be.above(0);
-		expect(
-			document.querySelector('article').outerHTML,
-			'No `header` tag was found inside a `article` tag'
-		).to.match(/<header>[\s\S]*?<\/header>/);
+			article,
+			'No `header` tag was found inside an `article` tag'
+		).to.contain('header');
 	});
 
 	it('contains an `h4` tag inside a `aside` tag', () => {
-		expect(
-			document.querySelectorAll('aside').length,
-			'No `aside` tag was found'
-		).to.be.above(0);
-		expect(
-			document.querySelector('aside').outerHTML,
-			'No `h4` tag was found inside a `aside` tag'
-		).to.match(/<h4>[\s\S]*?<\/h4>/);
+		let aside = document.querySelector('aside');
+
+		expect(body, 'No `aside` tag was found').to.contain('aside');
+		expect(article, 'No `h4` tag was found inside an `aside` tag').to.contain(
+			'h4'
+		);
 	});
 
 	it('contains a `strong` tag and an `em` tag inside a `p` tag', () => {
-		expect(
-			document.querySelectorAll('p').length,
-			'No `p` tag was found'
-		).to.be.above(0);
-		expect(
-			document.querySelector('p').outerHTML,
-			'No `strong` tag was found inside a `p` tag'
-		).to.match(/<strong>[\s\S]*?<\/strong>/);
-		expect(
-			document.querySelector('p').outerHTML,
-			'No `em` tag was found inside a `p` tag'
-		).to.match(/<em>[\s\S]*?<\/em>/);
+		let p = document.querySelector('p');
+
+		expect(body, 'No `p` tag was found').to.contain('p');
+		expect(p, 'No `strong` tag was found inside a `p` tag').to.contain(
+			'strong'
+		);
+		expect(p, 'No `em` tag was found inside a `p` tag').to.contain('em');
 	});
 
 	it('contains a `summary` tag inside a `details` tag', () => {
+		let details = document.querySelector('details');
+
+		expect(body, 'No `details` tag was found').to.contain('details');
 		expect(
-			document.querySelectorAll('details').length,
-			'No `details` tag was found'
-		).to.be.above(0);
-		expect(
-			document.querySelector('details').outerHTML,
+			details,
 			'No `summary` tag was found inside a `details` tag'
-		).to.match(/<summary>[\s\S]*?<\/summary>/);
+		).to.contain('summary');
 	});
 
 	it('contains a `q` tag inside a `div` tag', () => {
-		expect(
-			document.querySelectorAll('div').length,
-			'No `div` tag was found'
-		).to.be.above(0);
-		expect(
-			document.querySelector('div').outerHTML,
-			'No `q` tag was found inside a `div` tag'
-		).to.match(/<q>[\s\S]*?<\/q>/);
+		let div = document.querySelector('div');
+
+		expect(body, 'No `div` tag was found').to.contain('div');
+		expect(div, 'No `q` tag was found inside a `div` tag').to.contain('q');
 	});
 
 	it('contains a `del` tag and an `ins` tag inside an `address` tag', () => {
-		expect(
-			document.querySelectorAll('address').length,
-			'No `address` tag was found'
-		).to.be.above(0);
-		expect(
-			document.querySelector('address').outerHTML,
-			'No `del` tag was found inside a `address` tag'
-		).to.match(/<del>[\s\S]*?<\/del>/);
-		expect(
-			document.querySelector('address').outerHTML,
-			'No `ins` tag was found inside a `address` tag'
-		).to.match(/<ins>[\s\S]*?<\/ins>/);
+		let address = document.querySelector('address');
+
+		expect(body, 'No `address` tag was found').to.contain('address');
+		expect(address, 'No `del` tag was found inside a `address` tag').to.contain(
+			'del'
+		);
+		expect(address, 'No `ins` tag was found inside a `address` tag').to.contain(
+			'ins'
+		);
 	});
 
 	it('contains a `mark` tag inside an `h3` tag inside a `section` tag', () => {
+		let section = document.querySelector('section');
+		let h3 = document.querySelector('h3');
+
+		expect(body, 'No `section` tag was found').to.contain('section');
+		expect(section, 'No `h3` tag was found inside a `section` tag').to.contain(
+			'h3'
+		);
+		expect(h3, 'No `mark` tag was found inside a `h3` tag').to.contain('mark');
+
 		expect(
-			document.querySelectorAll('section').length,
-			'No `section` tag was found'
-		).to.be.above(0);
-		expect(
-			document.querySelector('section').outerHTML,
-			'No `h3` tag was found inside a `section` tag'
-		).to.match(/<h3>[\s\S]*?<\/h3>/);
-		expect(
-			document.querySelector('h3').outerHTML,
-			'No `mark` tag was found inside a `h3` tag'
-		).to.match(/<mark>[\s\S]*?<\/mark>/);
-		expect(
-			document.querySelector('section').outerHTML,
+			section,
 			'Create a `mark` tag inside an `h3` tag inside an `section` tag'
-		).to.match(/<mark>[\s\S]*?<\/mark>/);
-		expect(
-			document.querySelector('section').outerHTML,
-			'The `mark` is not nested correctly within `h3`'
-		).to.match(/<h3>[\s\S]*?<mark>[\s\S]*?<\/mark>[\s\S]*?<\/h3>/);
+		).to.contain('mark');
 	});
 });
